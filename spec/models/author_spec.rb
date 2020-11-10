62588e029d51f5adb62f184f034e81b345135478
Paula <paula.klinke@student.hpi.de>
2020-11-10 11:39:33 +0100

    Fixing test for #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "New author page", type: :feature do
 
   it "should have attributes and a concatinated attribute name" do
-    author = Author.new('Alan', 'Turing', 'example.de')
+    author = Author.new(first_name:'Alan', last_name:'Turing', homepage:'example.de')
     expect(author.first_name).to eq('Alan')
     expect(author.last_name).to eq('Turing')
     expect(author.homepage).to eq('example.de')
