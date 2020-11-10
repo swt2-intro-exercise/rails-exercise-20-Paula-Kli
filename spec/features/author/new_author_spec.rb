6ed566d2a27ca5a8336fabcb99b995080e87a196
Paula <paula.klinke@student.hpi.de>
2020-11-10 11:55:33 +0100

    Failing test for #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,5 +13,12 @@ it "should have text inputs for an author's first name, last name, and homepage"
   expect(page).to have_field('author[last_name]')
   expect(page).to have_field('author[homepage]')
 end
+it "when a user enters an author that one should be found in the database" do
+  visit new_author_path
+  page.fill_in 'author[first_name]', with: 'Edsger'
+  page.fill_in 'author[last_name]', with: 'Dijkstra'
+  page.fill_in 'author[homepage]', with: 'https://de.wikipedia.org/wiki/Edsger_W._Dijkstra'
+  find('input[type="submit"]').click
+end
 
 end
