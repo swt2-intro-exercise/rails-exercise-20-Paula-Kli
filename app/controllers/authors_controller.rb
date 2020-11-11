96b710dc4128ada9a51a67f9feab4192f0dddd43
Paula <paula.klinke@student.hpi.de>
2020-11-11 22:04:47 +0100

    Fixing test for #5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,6 +6,9 @@ class AuthorsController < ApplicationController
     @author.save
     redirect_to root_path, notice: 'Success!'
   end
+  def show
+    @author = Author.find(params[:id])
+  end
   private
     def article_params
       params.require(:author).permit(:first_name, :last_name, :homepage)
