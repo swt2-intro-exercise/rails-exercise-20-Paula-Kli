ccd52cb9806f9eeeae67ed8217fe93210bfd6c5a
Paula <paula.klinke@student.hpi.de>
2020-11-12 12:11:15 +0100

    Add New Button to index page

--- spec/models/authorIndex_spec.rb
+++ spec/models/authorIndex_spec.rb
@@ -5,6 +5,7 @@ describe "See index page for all authors", type: :feature do
     visit authors_path
     expect(page).to have_text("Name")
     expect(page).to have_text("Homepage")
+    expect(page).to have_link 'New', href: new_author_path
   end
-  
+
 end
