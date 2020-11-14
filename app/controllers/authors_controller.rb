08873c144ef4799b0bd95972631bce3f6c056bc5
Paula <paula.klinke@student.hpi.de>
2020-11-14 20:26:03 +0100

    Adding an edit view for authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,16 +2,27 @@ class AuthorsController < ApplicationController
   def new
     @author = Author.new
   end
+
   def edit
     @author = Author.find(params[:id])
   end
 
   def create
-    @author = Author.new(article_params)
+    @author = Author.new(authors_params)
     if @author.save
       redirect_to  @author
     else
       render 'new'
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(authors_params)
+      redirect_to @author
+    else
+      render 'edit'
     end
   end
 
@@ -22,8 +33,9 @@ class AuthorsController < ApplicationController
   def index
     @author = Author.all
   end
+
   private
-    def article_params
+    def authors_params
       params.require(:author).permit(:first_name, :last_name, :homepage)
     end
 end
