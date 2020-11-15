b4311aeb08d372fafe1405b6c54b6941469ce3c8
Paula <paula.klinke@student.hpi.de>
2020-11-15 20:16:20 +0100

    Failing test for authors related to a paper

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -18,4 +18,9 @@ RSpec.describe Paper, type: :model do
     paper = Paper.new(title: 'Potsdam', venue: 'Berlin', year: 20.5)
     expect(paper).to_not be_valid
   end
+
+  it "should have an empty list of authors" do
+    paper = Paper.new(title: 'Potsdam',venue: 'Potsdam', year: 2020)
+    expect(paper.authors).to be_empty
+  end
 end
