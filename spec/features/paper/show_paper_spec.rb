f760d068d2939077edbc29acbd1ddc7a4b44e790
Paula <paula.klinke@student.hpi.de>
2020-11-16 10:33:53 +0100

    Failing test for #10

---
+++ spec/features/paper/show_paper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "See details page for paper", type: :feature do
+
+  it "Should have a details page" do
+    @testPaper = FactoryBot.create :paper
+    visit paper_path(@testPaper)
+    expect(page).to have_text(@testPaper.title)
+    expect(page).to have_text(@testPaper.venue)
+    expect(page).to have_text(@testPaper.year)
+    expect(page).to have_text(@testPaper.authors)
+  end
+end
