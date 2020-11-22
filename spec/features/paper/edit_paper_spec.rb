ec2e64d800d98e4edfd9dc387b52b636ffcd1d70
Paula <paula.klinke@student.hpi.de>
2020-11-22 11:31:32 +0100

    Adding selection box for authors in paper editing form

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -5,7 +5,7 @@ describe "Edit paper should render", type: :feature do
   it "should have a field to select a matching author" do
     @testPaper = FactoryBot.create :paper
     @testAuthor = FactoryBot.create :author
-    visit edit_author_path(@testPaper)
-    page.select @testAuthor.name, from: 'Author'
+    visit edit_paper_path(@testPaper)
+    expect(page).to have_select('paper[author_ids]', type: :hidden)
   end
 end
